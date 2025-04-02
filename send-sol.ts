e4e85cd0a2fcae602b6a974efad56d3f7aeda9da
0xataru <0xataru@proton.me>
2025-04-03 00:16:44 +0300

    Remove unused import of sendAndConfirmRawTransaction from send-sol.ts

--- send-sol.ts
+++ send-sol.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, sendAndConfirmRawTransaction, sendAndConfirmTransaction, SystemProgram, Transaction } from "@solana/web3.js";
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction } from "@solana/web3.js";
 
 let privateKey = process.env.SECRET_KEY;
 let recipientPublicKey = process.env.RECIPIENT_PUBLIC_KEY;
